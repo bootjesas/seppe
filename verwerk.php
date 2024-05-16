f851e268e1d260fa9a99d4ef08da36148c9fba29
Bo Sas <bo.sas@telenet.be>
2024-05-16 22:20:40 +0200

    Eerste commit

---
+++ verwerk.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $naam = $_POST['name'];
+    $email = $_POST['Email'];
+    $onderwerp = $_POST['subject'];
+    $bericht = $_POST['message'];
+
+    $to = 'botje.sas@gmail.com';
+    $subject = 'Contactformulier - ' . $onderwerp;
+    $message = 'Naam: ' . $naam . "\r\n" .
+               'E-mail: ' . $email . "\r\n" .
+               'Bericht: ' . $bericht;
+
+    $headers = 'From: ' . $email . "\r\n" .
+               'Reply-To: ' . $email . "\r\n" .
+               'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+}
+?>
